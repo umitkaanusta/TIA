8f197c6590be989c85b770ae7e6aafec1b85eb51
Ümit Kaan Usta <53405015+umitkaanusta@users.noreply.github.com>
2020-06-30 23:14:55 +0300

    moved stemming_tokenizer function

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
 from tia.routes import app
-from nltk.stem import PorterStemmer
-from nltk import word_tokenize
 
-
-def stemming_tokenizer(text):
-    # This is here for nlp models (will be relocated)
-    stemmer = PorterStemmer()
-    return [stemmer.stem(w) for w in word_tokenize(text)]
-
+from tia.stalk.util import stemming_tokenizer
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
